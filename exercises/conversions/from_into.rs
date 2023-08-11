1377626fd12439335c85e837bd257a028ba89a5f
Kai Wang <charles.wangkai@gmail.com>
2023-08-11 12:03:43 +0800

    from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.find(',') {
+            Some(comma_index) => {
+                let name = String::from(&s[..comma_index]);
+                if name.is_empty() {
+                    return Person::default();
+                }
+
+                match s[comma_index + 1..].parse() {
+                    Ok(age) => Person { name, age },
+                    Err(_) => Person::default(),
+                }
+            }
+            None => Person::default(),
+        }
     }
 }
 
